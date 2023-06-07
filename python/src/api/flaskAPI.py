a01656a25b59c20089d7fef2c83ae28ec3aa4b6d
LazaroAlPaka <jfkoepper@googlemail.com>
2023-06-07 17:14:50 +0200

    feat: added first endpoint to pass json data to db

--- python/src/api/flaskAPI.py
+++ python/src/api/flaskAPI.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
-
+import json
 
 # create the extension
 db = SQLAlchemy()
@@ -12,18 +12,48 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////home//void/test.db"
 # initialize the app with the extension
 db.init_app(app)
 
+# Create database with its table and columns
 class Temperature(db.Model):
 
     __tablename__ = "data"
 
-    zeitpunkt = db.Column(db.TIMESTAMP , primary_key=True)
+    zeitpunkt = db.Column(db.INTEGER , primary_key=True)
     sensor = db.Column(db.VARCHAR(20))  
     temperatur = db.Column(db.FLOAT)
 
-    def __init__(self, sensor, temperatur):
-        self.zeitpunkt = db.time.time()
+    def __init__(self, zeitpunkt, sensor, temperatur):
+        self.zeitpunkt = zeitpunkt
         self.sensor = sensor
         self.temperatur = temperatur
 
 with app.app_context():
+    db.drop_all()
     db.create_all()
+
+# Endpoint
+
+@app.route("/test")
+
+def Hello():
+    return "Hello World"
+
+# Request JSON
+
+@app.route('/json_test', methods=['POST'])
+def handle_json():
+    #data = json.loads(request.json)
+    data = request.json
+    print(data)
+    return_code = 200
+    return_message = "Saved succesfully"
+    try:
+        post = Temperature(**data)
+        db.session.add(post)
+        db.session.commit()
+
+    except Exception as e:
+        return_code = 400
+        return_message = str(e)
+
+    finally:
+        return return_message, return_code   
