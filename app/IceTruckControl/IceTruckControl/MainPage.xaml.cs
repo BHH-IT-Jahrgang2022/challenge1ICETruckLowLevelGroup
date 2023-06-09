2c4aea7c0c820e066f283b6a0df9d5f142e72c9e
Pascal Boehler <pascal@bembel.dev>
2023-06-09 12:53:39 +0200

    feat: Implemented sensor status und fan / vent control tiles visually

--- app/IceTruckControl/IceTruckControl/MainPage.xaml.cs
+++ app/IceTruckControl/IceTruckControl/MainPage.xaml.cs
@@ -1,10 +1,36 @@
 ﻿namespace IceTruckControl;
 
 public partial class MainPage : ContentPage
-{	
+{
+
+	int tempSet = -17; // just a temp value thats not possible
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        updateTempSetView();
+    }
+
+
+    private void minTempBtn_Clicked(object sender, EventArgs e)
+    {
+        tempSet--;
+        updateTempSetView();
+    }
+
+    private void pluTempBtn_Clicked(object sender, EventArgs e)
+    {
+        tempSet++;
+        updateTempSetView();
+    }
+
+    private void updateTempSetView()
+    {
+        lblTempSet.Text = $"{tempSet}°C";
+    }
 }
 
