00c7101a4b8913ce7be51edf9a21b5e6e50841c8
Pascal Boehler <pascal@bembel.dev>
2023-06-11 18:13:08 +0200

    feat: Created Sensor cs model

---
+++ app/IceTruckControl/IceTruckControl/Models/Sensor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTruckControl.Models
+{
+    public class Sensor
+    {
+        public int SensorId { get; private set; }
+        public String SensorName { get; private set; }
+        public float CurrentTempMeasured { get; set; }
+        public DateTime TimeRecorded { get; set; }
+        public Sensor(String SensorName)
+        {
+            this.SensorName = SensorName;
+        }
+        public void SetNewMeasurement(float TempMeasured)
+        {
+            TimeRecorded = DateTime.Now;
+            CurrentTempMeasured = TempMeasured;
+            Debug.WriteLine($"Measured {TempMeasured}°C at {TimeRecorded}");
+        }
+
+    }
+}
