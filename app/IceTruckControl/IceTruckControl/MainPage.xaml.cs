3f61312a4020983fcdf1651ea510879222b77478
Pascal Boehler <pascal@bembel.dev>
2023-06-12 11:42:21 +0200

    feat: Worked on data structure and added some limits

--- app/IceTruckControl/IceTruckControl/MainPage.xaml.cs
+++ app/IceTruckControl/IceTruckControl/MainPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿namespace IceTruckControl;
+﻿using System.Diagnostics;
+
+namespace IceTruckControl;
 
 public partial class MainPage : ContentPage
 {
 
-	int tempSet = -17; // just a temp value thats not possible
+	int tempSet = -271; // just a temp value thats not possible
 	public MainPage()
 	{
 		InitializeComponent();
@@ -18,14 +20,35 @@ public partial class MainPage : ContentPage
 
     private void minTempBtn_Clicked(object sender, EventArgs e)
     {
-        tempSet--;
-        updateTempSetView();
+        if (tempSet > -273)
+        {
+            tempSet--;
+            updateTempSetView();
+        } /*else if (tempSet > -273.15) {
+            Debug.WriteLine("ERROR: This value is not possible for the hardware");
+            DisplayAlert("Temp not possible", "Due to Hardware limitations, please stick to a temperature greater than -40", "ok");
+        } */
+        else 
+        {
+            DisplayAlert("PHYSICS BROKE NOW", 
+                        "Hey! Stop breaking physics! It's not possible to cool something under the physical limit! You noop!", 
+                        "Physics does not count for me!");
+        }
+        
     }
 
     private void pluTempBtn_Clicked(object sender, EventArgs e)
     {
-        tempSet++;
-        updateTempSetView();
+        if (tempSet < 80) 
+        {
+            tempSet++;
+            updateTempSetView();
+        } else 
+        {
+            Debug.WriteLine("ERROR: This value literally is not possible");
+            DisplayAlert("Temp not possible", "Due to Hardware limitations, please stick to a temperature smaller than 80", "ok");
+        }
+        
     }
 
     private void updateTempSetView()
