44205b9976418daacbff0831c011483564c195d1
Pascal Boehler <pascal@bembel.dev>
2023-06-12 09:45:49 +0200

    feat: Created json sample data

--- app/IceTruckControl/IceTruckControl/Models/Sensor.cs
+++ app/IceTruckControl/IceTruckControl/Models/Sensor.cs
@@ -7,16 +7,20 @@ using System.Threading.Tasks;
 
 namespace IceTruckControl.Models
 {
+
+    // TODO: Decide on which way the model should work and if a DataHandler is needed
     public class Sensor
     {
         public int SensorId { get; private set; }
         public String SensorName { get; private set; }
         public float CurrentTempMeasured { get; set; }
+        // TODO: Do some Unix Timestamp Magic in set method
         public DateTime TimeRecorded { get; set; }
         public Sensor(String SensorName)
         {
             this.SensorName = SensorName;
         }
+        
         public void SetNewMeasurement(float TempMeasured)
         {
             TimeRecorded = DateTime.Now;
