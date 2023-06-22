69125bdce02a4396b1095a51c40c0cfc9994f0db
R-HOFF <r.hoffbuhr@stud.bhh.de>
2023-06-22 11:43:37 +0200

    feat: added additional log levels and removed unecessary challenge parameter (-c) works by itself now

--- python/src/mqtt/start.py
+++ python/src/mqtt/start.py
@@ -8,7 +8,7 @@ from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument("-t", "--temperature", type=int)
 parser.add_argument("-l", "--logging", type=str)
-parser.add_argument("-c", "--challenge", type=str)
+parser.add_argument("-c", "--challenge", action='store_true')
 
 args = parser.parse_args()
 target_temp = args.temperature
@@ -19,11 +19,14 @@ if log_level == None:
     logging.basicConfig(level=logging.CRITICAL)
 elif log_level == "info":
     logging.basicConfig(level=logging.INFO)
+elif log_level == "warning":
+    logging.basicConfig(level=logging.WARNING)
+elif log_level == "error":
+    logging.basicConfig(level=logging.ERROR)
 
 is_challenge = False
 if args.challenge != None:
-    is_challenge = args.challenge == "yes" or args.challenge == "y"
-
+    is_challenge = args.challenge
 if is_challenge:
     mqtt_controller.start_challenge(target_temp=target_temp)
 else:
